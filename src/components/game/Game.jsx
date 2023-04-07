1d70b564ea0379dd8799857ce9b26797657a1d78
Jessica Lopes <lopes.jessica1988@gmail.com>
2023-04-07 09:07:15 -0300

    Criado Placar

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -3,6 +3,7 @@ import styles from './Game.module.css'
 
 import GameOption from '../gameOption/GameOption'
 import GameInfo from '../gameInfo/GameInfo'
+import Score from '../score/Score'
 
 const winnerTable = [
   [0, 1, 2],
@@ -68,6 +69,7 @@ function Game () {
   }, [winner])
 
   return (
+  <>
     <div className={styles.gameContent}>
        <div className={styles.game}> {
         gameState.map((value, pos) =>
@@ -88,6 +90,8 @@ function Game () {
          isDraw={draw}
        />
     </div>
+    <Score />
+  </>
   )
 }
 
